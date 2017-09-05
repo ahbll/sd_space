90255f22342564d76177a9b85c889027aef34a22
ll <925783178@qq.com>
2017-09-05 09:57:26 +0800

    maven-多模块项目初始架构

---
+++ space_db/src/main/java/org/space_db/App.java
@@ -0,0 +1,13 @@
+package org.space_db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
